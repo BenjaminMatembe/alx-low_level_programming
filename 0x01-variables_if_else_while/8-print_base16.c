9f45013fd5e3eb1ce834bc5f99149371415b467b
BenjaminMatembe <matembebenjamin@gmail.com>
2022-03-11 12:59:08 -0800

    mm

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,16 +1,9 @@
 #include <stdio.h>
-
-
-
 /**
+ *main - Prints numbers between 0 to 9 and letters between a to f.
  *
- *  * main - Prints numbers between 0 to 9 and letters between a to f.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
+ *Return: Always 0 (Success)
+ */
 
 int main(void)
 
