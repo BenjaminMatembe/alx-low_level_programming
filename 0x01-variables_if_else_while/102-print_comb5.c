d04fb45157986a8392963dc3a80b7b053b15ad4c
BenjaminMatembe <matembebenjamin@gmail.com>
2022-03-11 12:42:02 -0800

    h

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -3,15 +3,10 @@
 
 
 /**
+ *main - Prints numbers between 00 to 99.
  *
- *  * main - Prints numbers between 00 to 99.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ *Return: Always 0 (Success)
+ */
 int main(void)
 
 {
