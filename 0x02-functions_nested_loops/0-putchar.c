bab60cee5489ea38ca7f60de398d09a40c689388
BenjaminMatembe <88074420+BenjaminMatembe@users.noreply.github.com>
2022-03-15 20:38:19 +0300

    Update 0-putchar.c

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,39 +1,22 @@
+#include <stdio.h>
 #include "main.h"
 
-
-
 /**
- *
- *  * main - Prints Holberton as a message.
- *
- *   *
- *
- *    * Return: Always 0 (Success)
- *
- *     */
-
+ * main -  prints Holberton, followed by a new line
+ * Return: Always 0 (Success)
+ */
 int main(void)
-
 {
+	_putchar('H');
+	_putchar('o');
+	_putchar('l');
+	_putchar('b');
+	_putchar('e');
+	_putchar('r');
+	_putchar('t');
+	_putchar('o');
+	_putchar('n');
+	_putchar('\n');
 
-		int str[] = {72, 111, 108, 98, 101, 114, 116, 111, 110};
-
-			int count, sz;
-
-
-
-				sz = sizeof(str) / sizeof(int);
-
-					for (count = 0; count < sz; count++)
-
-							{
-
-										_putchar(str[count]);
-
-											}
-
-						_putchar('\n');
-
-							return (0);
-
+	return (0);
 }
